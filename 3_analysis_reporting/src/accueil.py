3038b5615c20a450efb6c794c880038b81724917
Ahmed Tioual <91097298+AhmedTioual@users.noreply.github.com>
2025-10-15 21:28:19 +0200

    Update accueil.py

--- 3_analysis_reporting/src/accueil.py
+++ 3_analysis_reporting/src/accueil.py
@@ -170,10 +170,10 @@ def show_accueil():
     # Call the function inside the first column
     with col1:
         
-        st.subheader("Job Offers Map")
+        st.subheader("Carte des offres d'emploi")
         st_data = st_folium(plot_offers_map(df), width=600, height=500)
     with col2:
-        st.subheader("Top 10 Communes with Most Job Offers")
+        st.subheader("Top 10 des communes offrant le plus d'offres d'emploi")
         st.plotly_chart(plot_top_communes(df, top_n=10, height=500, width=600), use_container_width=False)
 
     st.markdown("<hr>", unsafe_allow_html=True)
@@ -182,8 +182,8 @@ def show_accueil():
     col1, col2 = st.columns(2)
 
     with col1:
-        st.subheader("Job Offers by Contract Type")
+        st.subheader("Offres d'emploi par type de contrat")
         st.plotly_chart(plot_contract_distribution(df), use_container_width=False)
     with col2:
-        st.subheader("Job Offers Over Time")
+        st.subheader("Offres d'emploi au fil du temps")
         st.plotly_chart(plot_offers_per_month(df), use_container_width=False)
